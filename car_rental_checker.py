08b03804a55d4c6336ac721ae3f63ccb07ab0655
Christianihechi <Christianihechi@gmail.com>
2023-06-08 11:59:20 +0100

    Update car_rental_checker.py

--- car_rental_checker.py
+++ car_rental_checker.py
@@ -1,16 +1,25 @@
-# Write a program that asks the user what kind of rental car they would like.
-# Print a message about that car, such as Let me see if I can find you a Subaru.
-
+# Ask the user for their choice of rental car.
 car_to_rent = input('What kind of car would you like to rent?\n')
+
+# Ask the user if they made a typo error or if they want to quit.
 mistake = input("Did you make a typo error?\n"
-                "Please type Yes, or No accordingly.\n"
+                "Please type 'Yes' or 'No' accordingly.\n"
                 "Type 'quit' if you are not renting today.\n")
+
+# Start an infinite loop to handle user input.
 while True:
+    # Check if the user wants to quit.
     if mistake.lower() == "quit":
         print("We are sorry to see you go.")
+        break
+
+    # Check if the user made a typo error.
+    if mistake.lower() == 'yes':
+        # Prompt the user to enter the correct car information.
+        car_correction = input("Please enter the correct car info.\n")
+        print(f"Great choice! Please wait while I search for a {car_correction.title()} car.")
+    elif mistake.lower() == 'no':
+        print(f"Great choice! Please wait while I search for a: {car_to_rent.title()} car.")
+        
+    # Exit the loop after processing the input.
     break
-if mistake.lower() == 'yes':
-    car_correction = input("Please enter the correct car info.\n")
-    print(f"Great choice! Please wait while I search for a {car_correction.title()} car.")
-elif mistake.lower() == 'no':
-    print(f"Great choice! Please wait while I search for a: {car_to_rent.title()} car.")
