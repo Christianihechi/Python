557add5b802640abe67e3e075f1289b5880eb10a
Christianihechi <Christianihechi@gmail.com>
2023-06-08 12:40:19 +0100

    Update restaurant_sitting.py

--- restaurant_sitting.py
+++ restaurant_sitting.py
@@ -1,6 +1,7 @@
-# Restaurant Seating
-
+# Get the number of people in the dinner group from the user
 restaurant_seating = int(input("How many people are in your dinner group? \n"))
+
+# Check if the dinner group exceeds the seating capacity
 if restaurant_seating > 8:
     print("Sorry, please wait for the next available table!")
 else:
