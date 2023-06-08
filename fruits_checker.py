08382ffcb69a9591aed87a869c6d94b8cce73901
Christianihechi <Christianihechi@gmail.com>
2023-06-08 12:01:16 +0100

    Update fruits_checker.py

--- fruits_checker.py
+++ fruits_checker.py
@@ -1,21 +1,24 @@
-# Create a list of fruits and ask the user to enter the name of a fruit.
-# Use a While loop to check if the entered fruit exists in the list.
-# If it does, print a message saying it's a healthy choice.
-# If it doesn't, ask the user to try again.
-# Repeat this process until the user enters a valid fruit or enters 'quit' to exit.
-
+# Create a list of fruits.
 fruits = ['mango', 'watermelon', 'pineapple', 'pear', 'lemon', 'cashew', 'orange',
           'grape', 'guava', 'pawpaw', 'lime', 'coconut', 'sugarcane', 'pepper',
-          'okra', 'potato', 'yam', 'casava', 'kola', 'groundnut', 'maize', 'corn' ]
+          'okra', 'potato', 'yam', 'casava', 'kola', 'groundnut', 'maize', 'corn']
 
+# Set the initial value for the loop control variable.
 active = True
+
+# Start a while loop to repeatedly ask the user for their favorite fruit.
 while active:
-    user_input = input("What is your favourite fruit?\n"
-                       "To stop, type 'quit'\n")
+    # Ask the user to enter their favorite fruit or 'quit' to exit.
+    user_input = input("What is your favorite fruit?\nTo stop, type 'quit'\n")
+
+    # Check if the user's input exists in the list of fruits.
     if user_input.lower() in fruits:
-        print(f"Great choice! {user_input} is a available and is very healthy!")
+        print(f"Great choice! {user_input} is available and is very healthy!")
         active = False
+    # Check if the user wants to quit.
     elif user_input.lower() == 'quit':
         active = False
+    # If the input is not a valid fruit or 'quit', ask the user to try again.
     else:
-        print("Try again !")
+        print("Try again!")
+
