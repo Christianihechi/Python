28d43111018e1cb4f52a249dd457dd75ff47c4a7
Christianihechi <Christianihechi@gmail.com>
2023-06-08 13:16:59 +0100

    Update valid_integer_square_root.py

--- valid_integer_square_root.py
+++ valid_integer_square_root.py
@@ -1,24 +1,21 @@
-# Write a program that asks the user to enter a number.
-# Use a while loop to keep asking for a number until the user enters a valid integer.
-# Once a valid number is entered, print a message displaying the square of that number.
-
-active = True
-while active:
-    user_input = input("Enter a number to check it's square:\n")
-    if not user_input.isdigit():
-        print("Please enter a valid integer.\n")
-    else:
-        user_input = int(user_input)
-        print(f"Square of {user_input} is :\n {user_input ** 2}")
-
-# Using the Try and Catch procedure.
-
-active = True
+        active = True
 while active:
     try:
+        # Prompt the user to enter a number
         user_input = input("Enter a number to check its square:\n")
-        user_input = float(user_input)
-        print("The square is ", (user_input ** 2))
+        
+        # Attempt to convert the user input to an integer
+        user_input = int(user_input)
+        
+        # Calculate the square of the number
+        square = user_input ** 2
+        
+        # Print the result
+        print(f"The square of {user_input} is: {square}")
+        
+        # Set active to False to exit the loop
         active = False
     except ValueError:
-        print("It seems you entered an invalid input. Numbers only !")
+        # If a ValueError is raised, the input is not a valid integer
+        # Display an error message and continue the loop to prompt for a valid input
+        print("Please enter a valid integer.\n")
