adefa06025f0bad86638dc71dec631d8c9ab98a3
Christianihechi <Christianihechi@gmail.com>
2023-06-08 12:00:04 +0100

    Update check_if_age_is_18.py

--- check_if_age_is_18.py
+++ check_if_age_is_18.py
@@ -1,6 +1,10 @@
-# Check age if it's 18 or less
+# Ask the user for their age.
 age = input("How old are you? ")
+
+# Convert the age to an integer.
 age = int(age)
+
+# Check if the age is 18 or higher.
 if age >= 18:
     print("You can proceed!")
 else:
