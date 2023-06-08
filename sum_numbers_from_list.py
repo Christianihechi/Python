1d87a33b0754f88c4d4640a75fd4f3040d662398
Christianihechi <Christianihechi@gmail.com>
2023-06-08 12:42:12 +0100

    Update sum_numbers_from_list.py

--- sum_numbers_from_list.py
+++ sum_numbers_from_list.py
@@ -1,16 +1,23 @@
-# Write a program that asks the user to enter a series of numbers.
-# Use a while loop to keep accepting input until the user enters a specific value, such as 0.
-# Then, calculate and display the sum of all the numbers entered.
-
+# Create an empty list to store the numbers
 sum_list = []
+
+# Set the loop to active
 active = True
 
+# Continue accepting numbers until the user enters 0
 while active:
-    num = int(input("Type a number and press Enter to allow you type the next number. "
-                    "Type 0 when you are ready to add all the numbers.\n"))
+    num = int(input("Type a number and press Enter to enter the next number. "
+                    "Type 0 when you are ready to calculate the sum.\n"))
+
+    # Check if the number is not 0
     if num != 0:
         sum_list.append(num)
     else:
+        # Calculate the sum of all the numbers entered
         total = sum(sum_list)
-        print(f"The SUM of the numbers is:\n{total}")
+
+        # Print the sum of the numbers
+        print(f"The sum of the numbers is: {total}")
+
+        # Set the loop to inactive and exit
         active = False
