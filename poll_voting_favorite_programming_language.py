8b5cc139d9791ebe4ba5701030aba0d682b3db5a
Christianihechi <Christianihechi@gmail.com>
2023-06-08 12:37:21 +0100

    Update poll_voting_favorite_programming_language.py

--- poll_voting_favorite_programming_language.py
+++ poll_voting_favorite_programming_language.py
@@ -1,21 +1,26 @@
-# Write a program that asks users to enter their favorite programming language.
-# Allow them to enter as many languages as they want.
-# When they're done, display the total number of votes for each language.
-
+# Create an empty dictionary to store the votes for each programming language.
 votes = {}
+
+# Set the initial value for the loop control variable.
 active = True
 
+# Start a while loop to collect the votes.
 while active:
+    # Ask the user to enter their favorite programming language.
     language = input("Enter your favorite programming language (or 'quit' to exit): ")
+
+    # Check if the user wants to quit.
     if language.lower() == 'quit':
         active = False
     else:
+        # Add the vote to the dictionary.
         if language in votes:
             votes[language] += 1
         else:
             votes[language] = 1
 
 print("\n--- Poll Results ---")
+
+# Print the poll results by iterating over the dictionary.
 for language, count in votes.items():
     print(f"{language.title()}: {count} votes")
-
