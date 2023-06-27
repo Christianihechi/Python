bd309c4942daa92edd3014f3bd6d818c757f7f59
Christianihechi <Christianihechi@gmail.com>
2023-06-27 07:13:59 +0100

    Added print statement before break Added code to catch value errors from input

--- random_number.py
+++ random_number.py
@@ -1,8 +1,3 @@
-# Write a program that generates a random number between 1 and 10.
-# The user should guess the number, and the program should provide feedback if the guess is too high or too low.
-# The program should continue to prompt the user for guesses
-# Until they correctly guess the number. 
-
 import random
 
 # Generate a random number between 1 and 10
@@ -10,15 +5,19 @@ random_number = random.randint(1, 10)
 
 # Start a while loop to prompt the user for guesses
 while True:
-    # Get the user's guess
-    guess = int(input("Guess a number between 1 and 10: "))
+    try:
+        # Get the user's guess
+        guess = int(input("Guess a number between 1 and 10: "))
 
-    # Check if the guess is correct
-    if guess == random_number:
-        break
+        # Check if the guess is correct
+        if guess == random_number:
+            print(f"Your guess, {guess} is correct!")
+            break
 
-    # Check if the guess is too low or too high
-    if guess > random_number:
-        print("The number is lower than that. Please guess again.")
-    elif guess < random_number:
-        print("The number is higher than that. Please guess again.")
+        # Check if the guess is too low or too high
+        if guess > random_number:
+            print("The number is lower than that. Please guess again.")
+        elif guess < random_number:
+            print("The number is higher than that. Please guess again.")
+    except ValueError:
+        print("That's probably not a number !")
